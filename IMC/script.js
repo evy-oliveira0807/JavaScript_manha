0df0ebeac83c15207ceee907389ddd2593a54e13
Evelyn Oliveira Ribeiro <53143873876@info.local>
2023-10-17 08:13:52 -0300

    finalizado calculadora IMC

--- IMC/script.js
+++ IMC/script.js
@@ -1,93 +1,111 @@
-const arrPessoas = []; //array vazio
+const arrPessoas = [];
 
-function calcular(e) {
-  e.preventDefault(); //capturar o evento de submit do formulário (para)
 
-  // pegar os dados do form
-  const nome = document.getElementById("nome").value.trim();
-  const altura = parseFloat(document.getElementById("altura").value);
-  const peso = parseFloat(document.getElementById("peso").value);
+function calcular(e){
+e.preventDefault();
 
-  if (isNaN(altura) || isNaN(peso) || nome.length == 0) {
-    alert("É necessário preecher os números corretamente");
-    return;
-  }
+    const nome = document.getElementById('nome').value.trim();
+    const altura = parseFloat(document.getElementById('altura').value)
+    const peso = parseFloat(document.getElementById('peso').value)
 
-  // calcular o imc
-  const imc = calcularImc(altura, peso);
-  // gera o texto da situação
-  const txtSituacao = retornaSituacao(imc);
+    if (isNaN(altura) || isNaN(peso) || nome.length == 0) {
+        alert("É necessário preencher os números corretamente!")
+        return;
+    }
+    //calcular o imc
+    const imc = calcularImc(altura, peso);
+    // gera o texto da situacao
+    const txtSituacao = retornaSituacao(imc)
 
-  // gera o objeto com os dados da pessoa
-  const pessoa = { nome, altura, peso, imc, situacao: txtSituacao }; //ojeto literal
+    //gera o objeto com os dados da pessoa
+    const pessoa = {
+        nome, altura, peso, imc, situacao: txtSituacao
+    };
+    //adiciona a pessoa na losta
+    arrPessoas.push(pessoa);
+    
+    //lista as pessoas
+    listarPessoas();
 
-  //adiciona a pessoa na lista
-  arrPessoas.push(pessoa);
 
-  // listar as pessoas
-  listarPessoas();
+
+
+    console.log(pessoa);
+
+
+    // console.log(nome);
+    // console.log(altura);
+    // console.log(peso);
+    // console.log(imc);
+    // console.log(situacao);
 }
 
-// return peso / altura ** 2;
-//     return peso / (altura * altura);
-function calcularImc(altura, peso) {
-  return peso / Math.pow(altura, 2);
+function calcularImc(altura, peso){
+
+    return peso / Math.pow(altura, 2)
+    return peso / (altura * altura);
+
+   
 }
 
-/*
-    Resultado	        Situação
-    Menor que 18.5      Magreza Severa
-    Entre 18.5 e 24.99	Peso normal
-    Entre 25 e 29.99	Acima do peso
-    Entre 30 e 34.99	Obesidade I
-    Entre 35 e 39.99	Obesidade II (severa)
-    Acima de 40	        Cuidado!!! else
-*/
+
 function retornaSituacao(imc) {
-  if (imc < 18.5) {
-    return "Magreza Severa!";
-  } else if (imc <= 24.99) {
-    return "Peso normal";
-  } else if (imc <= 29.99) {
-    return "Acima do peso";
-  } else if (imc <= 34.99) {
-    return "Obesidade I";
-  } else if (imc <= 39.99) {
-    return "Obesidade II";
-  } else {
-    return "Cuidado!";
-  }
+    if (imc < 18.5) {
+        return "Magreza severa"
+    }
+    else if ( imc <= 24.99) {
+        return "Peso normal"
+    }
+    else if (imc <= 29.99) {
+        return "Acima do peso"
+    }
+    else if (imc <= 34.99) {
+        return "Obesidade I"
+    }
+    else if(imc <= 39.99){
+        return "Obesidade II"
+    }
+    else {
+        return "Cuidado!"
+    }
+
+
+
+    // return 'situação'
 }
 
-// lista as pessoas na tabela
-function listarPessoas() {
-//   let textoQualquer = [];
+function listarPessoas(){
+    console.log(arrPessoas)
 
-//   textoQualquer.push("<p>EDUARDO</p>"); //0
-//   textoQualquer.push("<p>JONAS</p>"); //1
-//   textoQualquer.push("<p>CARLOS</p>"); //2
+    let template = "";
+    let tabela = document.getElementById("corpo-tabela")
+    arrPessoas.forEach(p =>{
+     template += `
+    <tr>
+    <th>${p.nome}</th>
+    <th>${p.altura}</th>
+    <th>${p.peso}</th>
+    <th>${p.imc}</th>
+    <th>${p.situacao}</th>
+    </tr>   
+    `;
+    })
+    tabela.innerHTML = template;
 
-//   let temp = "";
 
-//   textoQualquer.forEach((e) => {
-//     temp += e;
-//   });
+    //MANEIRA DIFERENTE
+    // let template = "";
 
-//   document.getElementById("teste").innerHTML = temp;
+    // arrPessoas.forEach((p) => {//preenche ou acumula os valorez na string
+    //     template += `
+    //     <tr>
+    //     <th>${p.nome}</th>
+    //     <th>${p.altura}</th>
+    //     <th>${p.peso}</th>
+    //     <th>${p.imc}</th>
+    //     <th>${p.situacao}</th>
+    //     </tr> `;
+    // });
 
-  let template = "";//string vazia
-
-  arrPessoas.forEach((p) => {//preenche ou acumula valores na sttring
-    template += `
-        <tr>
-            <td>${p.nome}</td>
-            <td>${p.altura}</td>
-            <td>${p.peso}</td>
-            <td>${p.imc.tofixed(2)}</td>
-            <td>${p.situacao}</td>
-        </tr>`;
-  });
-
-//injeta a string completa no html
-  document.getElementById('corpo-tabela').innerHTML = template;
+    // document.getElementById('corpo-tabela').innerHTML = template;
 }
